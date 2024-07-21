78edfcd44d7ab79b871c8014e6355eea5272ab1e
Arjun-Saleesh <arjunsaleesh@gmail.com>
2024-07-21 20:43:03 +0530

    Bank localstorage

---
+++ bank.js
@@ -0,0 +1,51 @@
+function submit(){
+    customer={
+        accnumber:accno.value,
+        username:uname.value,
+        password:passw.value
+
+    }
+    if(customer.accnumber=="" || customer.username=="" || customer.password==""){
+        alert("Please fill the missing fields")
+    }else{
+        if(customer.accnumber in localStorage){
+            alert("Already Exists")
+        }else{
+            localStorage.setItem(customer.accnumber,JSON.stringify(customer))
+                alert("Sucessfully added")
+                window.location='./login.html'
+        }
+    }
+}
+
+
+
+function login(){
+    let loginacno = document.getElementById('loginacno').value;
+    let loginpassw = document.getElementById('loginpassw').value;
+  
+    let customer = JSON.parse(localStorage.getItem(loginacno));
+  
+    if(loginacno=="" || loginpassw==""){
+      alert("Please fill the missing fields")
+    }else{
+      if(customer.accnumber ==loginacno && customer.password == loginpassw){
+        alert("Login successful")
+        localStorage.setItem('logined', loginacno)
+        window.location='./index.html'
+      }else{
+        alert("Invalid Account Number or Password")
+      }
+    }
+  }
+
+
+
+
+  
+
+
+
+
+
+
---
+++ withdraw.js
@@ -0,0 +1,49 @@
+let accnumber=localStorage.getItem('logined');
+let customer=JSON.parse(localStorage.getItem(accnumber));
+let username=customer.username;
+// usernamedisplay.innerHtml=username ?`Welcome ${username}` :"not found";
+
+
+
+
+function deposit(){
+    
+    if(!customer.balance){
+        customer.balance=0;   
+    }
+    let deposit=Number(depamount.value);
+    if(deposit <=0 || (deposit=="")){
+        alert("enter a amount")
+    }else{
+        customer.balance+=deposit;
+        localStorage.setItem(accnumber,JSON.stringify(customer))
+        alert(`Successfully added amount is ${deposit}`);
+        alert(`current bank balance is ${customer.balance}`)
+        // finalbalance.value=customer.balance;
+        balance()
+    }
+}
+
+function withdraw(){
+
+    let withdraw=withdrawamount.value;
+    if(withdraw<=0 || withdraw==''){
+        alert("Enter a correct amount to withdraw")
+    }else if(withdraw > customer.balance){
+            alert("Insufficient bank balance")
+        
+    }else{
+        customer.balance-=withdraw;
+        localStorage.setItem(accnumber,JSON.stringify(customer));
+        alert(`The withdraw amount is ${withdraw}`)
+        alert(`Current bank balance is ${customer.balance}`)
+        // finalbalance.value = customer.balance;
+        balance()
+    }
+
+}
+function balance(){
+   
+    finalbalance.value=customer.balance;
+    
+}
